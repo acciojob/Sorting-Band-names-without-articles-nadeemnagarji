eab4ae2a66ebd910e4aaf6d10bd5d8a1797a24ab
nadeemnagarji <129616695+nadeemnagarji@users.noreply.github.com>
2023-09-28 19:11:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,43 @@
 //your code here
 
+const bandNames = ['The Rolling Stones', 'The Beatles', 'Aerosmith', 'Led Zeppelin'];
+
+// Function to strip articles ('a', 'an', 'the') from a string and return the cleaned version
+function stripArticles(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
+}
+
+// Sort the band names while ignoring articles
+const sortedBandNames = bandNames.sort((a, b) => {
+  const nameA = stripArticles(a);
+  const nameB = stripArticles(b);
+  return nameA.localeCompare(nameB);
+});
+
+// Get the ul element by its id
+const ul = document.getElementById('bands');
+
+// Create li elements and add them to the ul
+sortedBandNames.forEach((bandName) => {
+  const li = document.createElement('li');
+  li.textContent = bandName;
+  ul.appendChild(li);
+});
+In this code:
+
+We define an array called bandNames that contains the list of band names.
+
+We create a stripArticles function that takes a name as input and removes any articles ('a', 'an', 'the') from the beginning of the name using a regular expression.
+
+We sort the bandNames array based on the cleaned band names, excluding articles, using the localeCompare method for lexicographic sorting.
+
+We retrieve the ul element with the id 'bands' using document.getElementById.
+
+We iterate through the sorted band names, create li elements for each one, and append them to the ul element.
+
+When you load this script, it will sort the band names in lexicographic order while ignoring articles and display them as a list in the ul element.
+
+
+
+
+
